1c8c0ed5b8acfc50df2dd38547b5fb8947b64663
Amarajah <naomi.ibeh69@gmail.com>
2023-07-19 21:45:21 +0000

    original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,3 +23,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, int, bytes]:
+        """Reading from Redis and recovering original type"""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ automatically parametrize Cache.get to str """
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ automatically parametrize Cache.get to int """
+        data = self._redis.get(key)
+        try:
+            data = int(value.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
