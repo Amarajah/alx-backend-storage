1ccfebfae1002268dc93ba0a81f4d51622d5e189
Amarajah <naomi.ibeh69@gmail.com>
2023-07-19 21:56:22 +0000

    lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,18 @@
 """Create a Cache class."""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional, Any
+
+iidef count_calls(method: Callable) -> Callable:
+    """ to count how many times methods of the Cache class are called """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapped function """
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
 
 
 class Cache:
@@ -15,6 +26,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[int, bytes, float, str]) -> str:
         """method that takes a data argument and returns a string.
         The method should generate a random key (e.g. using uuid),
@@ -46,3 +59,18 @@ class Cache:
         except Exception:
             data = 0
         return data
+
+def call_history(method: Callable) -> Callable:
+    """ store the history of inputs and outputs for a particular function """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapped function """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
