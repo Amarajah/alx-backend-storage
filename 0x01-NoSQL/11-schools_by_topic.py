b3290dca4429832676770deb4025b9997e6ec8ac
Amarajah <naomi.ibeh69@gmail.com>
2023-07-21 03:30:58 -0700

    topics

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,7 +5,4 @@ import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     """topic (string) will be topic searched"""
-    topic_list = []
-    for topic in mongo_collection.topic.find():
-        topic_list.append(topic)
-    return topic_list
+    return mongo_collection.find({"topics": topic})
